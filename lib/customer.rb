2cd8a9dcf12ba253227e5f0e527bea0a015c06a5
liorshine <shinelior@gmail.com>
2020-06-25 19:48:07 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,41 @@
 class Customer
+    require "pry"
+    attr_accessor :name, :age
   
+    @@all = []
+  
+    def initialize(name, age)
+      @name = name
+      @age = age
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+
+  def new_meal(waiter,total,tip)
+    Meal.new(waiter,self,total,tip)
+  end
+   def meals
+   Meal.all.select{|m|  m.customer == self}
+    end
+    def waiters
+        
+        meals.map{|m| 
+        m.waiter
+        }  
+        
+    end 
+    def best_tipper
+        all_tips = []
+        Meal.all.select{|x| 
+        all_tips << x.tip
+    }
+      max_tip =  all_tip.max
+      Meal.all.find{|x|
+          x.tip == max.tip 
+          x.customer
+      }
+    end
 end
